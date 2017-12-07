8dc1d38e00110479c8c35c47299713f32b752afb
Thiago Santos <thiagossantos@gmail.com>
2017-12-07 08:52:00 -0800

    grio: move to using encodebin for encoding

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ extern crate gstreamer;
 use gstreamer as gst;
 use gst::prelude::*;
 
+extern crate gstreamer_pbutils;
+use gstreamer_pbutils as pbutils;
+
+use pbutils::EncodingContainerProfileExt;
+
 extern crate glib;
 
 #[derive(Debug)]
@@ -41,26 +46,82 @@ impl Config {
     }
 }
 
+fn create_container_caps(format : &MediaFormat) -> gst::Caps {
+    match format {
+        &MediaFormat::MP4 => gst::Caps::from_string("video/quicktime, variant=(string)iso").unwrap(),
+        &MediaFormat::MKV => gst::Caps::from_string("video/x-matroska").unwrap(),
+        &MediaFormat::Unknown => gst::Caps::from_string("").unwrap()
+    }
+}
+
+fn create_video_caps(format : &MediaFormat) -> gst::Caps {
+    gst::Caps::from_string("video/x-h264").unwrap()
+}
+
+fn create_audio_caps(format : &MediaFormat) -> gst::Caps {
+    gst::Caps::from_string("audio/mpeg, mpegversion=(int)4").unwrap()
+}
+
+fn create_encoding_profile(config : &Config) -> pbutils::EncodingProfile {
+    let container_caps = create_container_caps(&config.output_format);
+    let video_caps = create_video_caps(&config.output_format);
+    let audio_caps = create_audio_caps(&config.output_format);
+
+    let encoding_profile = pbutils::EncodingContainerProfile::new("container", None, &container_caps, None);
+    let video_profile = pbutils::EncodingVideoProfile::new(&video_caps, None, None, 0);
+    let audio_profile = pbutils::EncodingAudioProfile::new(&audio_caps, None, None, 0);
+    encoding_profile.add_profile(&video_profile);
+    encoding_profile.add_profile(&audio_profile);
+
+    return encoding_profile.upcast();
+}
+
 fn transcode(config : Config) {
     let pipeline = gst::Pipeline::new("transcoder");
+    let filesink = gst::ElementFactory::make("filesink", None).unwrap();
+    let encodebin = gst::ElementFactory::make("encodebin", None).unwrap();
     let uridecodebin = gst::ElementFactory::make("uridecodebin", None).unwrap();
     uridecodebin.set_property("uri", &glib::Value::from(&config.input)).unwrap();
+    filesink.set_property("location", &glib::Value::from(&config.output)).unwrap();
 
     pipeline.add(&uridecodebin).unwrap();
+    pipeline.add(&encodebin).unwrap();
+    pipeline.add(&filesink).unwrap();
+
+    encodebin.link(&filesink).unwrap();
+
+    let encoding_profile = create_encoding_profile(&config);
+    encodebin.set_property("profile", &encoding_profile);
 
     let pipeline_clone = pipeline.clone();
+    let encodebin_clone = encodebin.clone();
     uridecodebin.connect_pad_added(move |_, src_pad| {
-        let queue = gst::ElementFactory::make("queue", None).unwrap();
-        let fakesink = gst::ElementFactory::make("fakesink", None).unwrap();
         let pipeline = &pipeline_clone;
+        let encodebin = &encodebin_clone;
 
+        let (is_audio, is_video) = {
+            let caps = src_pad.get_current_caps().unwrap();
+            let structure = caps.get_structure(0).unwrap();
+            let name = structure.get_name();
+
+            (name.starts_with("audio/"), name.starts_with("video/"))
+        };
+
+        let queue = gst::ElementFactory::make("queue", None).unwrap();
         pipeline.add(&queue).unwrap();
-        pipeline.add(&fakesink).unwrap();
-
-        queue.link(&fakesink).unwrap();
+        if is_video {
+            let enc_sink_pad = encodebin.get_request_pad("video_%u").unwrap();
+            let queue_src_pad = queue.get_static_pad("src").unwrap();
+            queue_src_pad.link(&enc_sink_pad);
+        } else if is_audio {
+            let enc_sink_pad = encodebin.get_request_pad("audio_%u").unwrap();
+            let queue_src_pad = queue.get_static_pad("src").unwrap();
+            queue_src_pad.link(&enc_sink_pad);
+        } else {
+            return;
+        }
 
         queue.sync_state_with_parent().unwrap();
-        fakesink.sync_state_with_parent().unwrap();
 
         let sink_pad = queue.get_static_pad("sink").unwrap();
         assert_eq!(src_pad.link(&sink_pad), gst::PadLinkReturn::Ok);
@@ -74,8 +135,8 @@ fn transcode(config : Config) {
         match msg.view() {
             MessageView::Eos(..) => break,
             MessageView::Error(err) => {
-                println!("Error from {}: {} ({:?})",
-                        msg.get_src().get_path_string(), err.get_error(),
+                println!("Error from {:?}: {} ({:?})",
+                        msg.get_src(), err.get_error(),
                         err.get_debug());
                 break;
             }
